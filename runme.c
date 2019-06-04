a8afb4a70bb7594990aa41453a03d8d0adb16348
Patrick Sanan <patrick.sanan@gmail.com>
2019-06-04 09:20:07 +0200

    cleanup

--- runme.c
+++ runme.c
@@ -11,7 +11,6 @@ Input parameters include:\n\
 #include <petsc/private/tsimpl.h>
 
 typedef struct {
-  Vec         solution;          /* global exact solution vector */
   PetscInt    m;                 /* total number of grid points */
   PetscReal   h;                 /* mesh width h = 1/(m-1) */
   Mat         A;                 /* RHS mat, used with IFunction interface */
@@ -20,8 +19,6 @@ typedef struct {
 
 extern PetscErrorCode InitialConditions(Vec,AppCtx*);
 extern PetscErrorCode RHSMatrixHeat(TS,PetscReal,Vec,Mat,Mat,void*);
-extern PetscErrorCode IFunctionHeat(TS,PetscReal,Vec,Vec,Vec,void*);
-extern PetscErrorCode IJacobianHeat(TS,PetscReal,Vec,Vec,PetscReal,Mat,Mat,void*);
 
 /* NEW: generic rollback impl */
 PetscErrorCode TSRollBack_Generic(TS ts)
@@ -108,11 +105,6 @@ int main(int argc,char **argv)
   PetscInt       m;
   PetscMPIInt    size;
   PetscReal      dt;
-  PetscBool      flg;
-
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-     Initialize program and set problem parameters
-     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
   ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
   ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
@@ -125,7 +117,6 @@ int main(int argc,char **argv)
   appctx.h        = 1.0/(m-1.0);
 
   ierr = VecCreateSeq(PETSC_COMM_SELF,m,&u);CHKERRQ(ierr);
-  ierr = VecDuplicate(u,&appctx.solution);CHKERRQ(ierr);
 
   ierr = TSCreate(PETSC_COMM_SELF,&ts);CHKERRQ(ierr);
   ierr = TSSetProblemType(ts,TS_LINEAR);CHKERRQ(ierr);
@@ -135,43 +126,10 @@ int main(int argc,char **argv)
   ierr = MatSetFromOptions(A);CHKERRQ(ierr);
   ierr = MatSetUp(A);CHKERRQ(ierr);
 
-  flg  = PETSC_FALSE;
-  ierr = PetscOptionsGetBool(NULL,NULL,"-use_ifunc",&flg,NULL);CHKERRQ(ierr);
-  if (!flg) {
-    appctx.A = NULL;
-    ierr = PetscOptionsGetBool(NULL,NULL,"-time_dependent_rhs",&flg,NULL);CHKERRQ(ierr);
-    if (flg) {
-      /*
-         For linear problems with a time-dependent f(u,t) in the equation
-         u_t = f(u,t), the user provides the discretized right-hand-side
-         as a time-dependent matrix.
-      */
-      ierr = TSSetRHSFunction(ts,NULL,TSComputeRHSFunctionLinear,&appctx);CHKERRQ(ierr);
-      ierr = TSSetRHSJacobian(ts,A,A,RHSMatrixHeat,&appctx);CHKERRQ(ierr);
-    } else {
-      /*
-         For linear problems with a time-independent f(u) in the equation
-         u_t = f(u), the user provides the discretized right-hand-side
-         as a matrix only once, and then sets the special Jacobian evaluation
-         routine TSComputeRHSJacobianConstant() which will NOT recompute the Jacobian.
-      */
-      ierr = RHSMatrixHeat(ts,0.0,u,A,A,&appctx);CHKERRQ(ierr);
-      ierr = TSSetRHSFunction(ts,NULL,TSComputeRHSFunctionLinear,&appctx);CHKERRQ(ierr);
-      ierr = TSSetRHSJacobian(ts,A,A,TSComputeRHSJacobianConstant,&appctx);CHKERRQ(ierr);
-    }
-  } else {
-    Mat J;
-
-    ierr = RHSMatrixHeat(ts,0.0,u,A,A,&appctx);CHKERRQ(ierr);
-    ierr = MatDuplicate(A,MAT_DO_NOT_COPY_VALUES,&J);CHKERRQ(ierr);
-    ierr = TSSetIFunction(ts,NULL,IFunctionHeat,&appctx);CHKERRQ(ierr);
-    ierr = TSSetIJacobian(ts,J,J,IJacobianHeat,&appctx);CHKERRQ(ierr);
-    ierr = MatDestroy(&J);CHKERRQ(ierr);
-
-    ierr = PetscObjectReference((PetscObject)A);CHKERRQ(ierr);
-    appctx.A = A;
-    appctx.oshift = PETSC_MIN_REAL;
-  }
+  appctx.A = NULL;
+  ierr = RHSMatrixHeat(ts,0.0,u,A,A,&appctx);CHKERRQ(ierr);
+  ierr = TSSetRHSFunction(ts,NULL,TSComputeRHSFunctionLinear,&appctx);CHKERRQ(ierr);
+  ierr = TSSetRHSJacobian(ts,A,A,TSComputeRHSJacobianConstant,&appctx);CHKERRQ(ierr);
 
   dt   = appctx.h*appctx.h/2.0;
   ierr = TSSetTimeStep(ts,dt);CHKERRQ(ierr);
@@ -203,92 +161,34 @@ int main(int argc,char **argv)
   ierr = TSDestroy(&ts);CHKERRQ(ierr);
   ierr = MatDestroy(&A);CHKERRQ(ierr);
   ierr = VecDestroy(&u);CHKERRQ(ierr);
-  ierr = VecDestroy(&appctx.solution);CHKERRQ(ierr);
   ierr = MatDestroy(&appctx.A);CHKERRQ(ierr);
 
   ierr = PetscFinalize();
   return ierr;
 }
 
-/* --------------------------------------------------------------------- */
-/*
-   InitialConditions - Computes the solution at the initial time.
-
-   Input Parameter:
-   u - uninitialized solution vector (global)
-   appctx - user-defined application context
-
-   Output Parameter:
-   u - vector with solution at initial time (global)
-*/
 PetscErrorCode InitialConditions(Vec u,AppCtx *appctx)
 {
   PetscScalar    *u_localptr,h = appctx->h;
   PetscErrorCode ierr;
   PetscInt       i;
 
-  /*
-    Get a pointer to vector data.
-    - For default PETSc vectors, VecGetArray() returns a pointer to
-      the data array.  Otherwise, the routine is implementation dependent.
-    - You MUST call VecRestoreArray() when you no longer need access to
-      the array.
-    - Note that the Fortran interface to VecGetArray() differs from the
-      C version.  See the users manual for details.
-  */
   ierr = VecGetArray(u,&u_localptr);CHKERRQ(ierr);
-
-  /*
-     We initialize the solution array by simply writing the solution
-     directly into the array locations.  Alternatively, we could use
-     VecSetValues() or VecSetValuesLocal().
-  */
   for (i=0; i<appctx->m; i++) u_localptr[i] = PetscSinScalar(PETSC_PI*i*6.*h) + 3.*PetscSinScalar(PETSC_PI*i*2.*h);
-
-  /*
-     Restore vector
-  */
   ierr = VecRestoreArray(u,&u_localptr);CHKERRQ(ierr);
 
   return 0;
 }
 
-/* --------------------------------------------------------------------- */
-/*
-   RHSMatrixHeat - User-provided routine to compute the right-hand-side
-   matrix for the heat equation.
-
-   Input Parameters:
-   ts - the TS context
-   t - current time
-   global_in - global input vector
-   dummy - optional user-defined context, as set by TSetRHSJacobian()
-
-   Output Parameters:
-   AA - Jacobian matrix
-   BB - optionally different preconditioning matrix
-   str - flag indicating matrix structure
-
-   Notes:
-   Recall that MatSetValues() uses 0-based row and column numbers
-   in Fortran as well as in C.
-*/
 PetscErrorCode RHSMatrixHeat(TS ts,PetscReal t,Vec X,Mat AA,Mat BB,void *ctx)
 {
-  Mat            A       = AA;                /* Jacobian matrix */
-  AppCtx         *appctx = (AppCtx*)ctx;     /* user-defined application context */
+  Mat            A       = AA;
+  AppCtx         *appctx = (AppCtx*)ctx;
   PetscInt       mstart  = 0;
   PetscInt       mend    = appctx->m;
   PetscErrorCode ierr;
   PetscInt       i,idx[3];
   PetscScalar    v[3],stwo = -2./(appctx->h*appctx->h),sone = -.5*stwo;
-
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-     Compute entries for the locally owned part of the matrix
-     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  /*
-     Set matrix rows corresponding to boundary data
-  */
 
   mstart = 0;
   v[0]   = 1.0;
@@ -299,10 +199,6 @@ PetscErrorCode RHSMatrixHeat(TS ts,PetscReal t,Vec X,Mat AA,Mat BB,void *ctx)
   v[0] = 1.0;
   ierr = MatSetValues(A,1,&mend,1,&mend,v,INSERT_VALUES);CHKERRQ(ierr);
 
-  /*
-     Set matrix rows corresponding to interior data.  We construct the
-     matrix one row at a time.
-  */
   v[0] = sone; v[1] = stwo; v[2] = sone;
   for (i=mstart; i<mend; i++) {
     idx[0] = i-1; idx[1] = i; idx[2] = i+1;
@@ -312,29 +208,5 @@ PetscErrorCode RHSMatrixHeat(TS ts,PetscReal t,Vec X,Mat AA,Mat BB,void *ctx)
   ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatSetOption(A,MAT_NEW_NONZERO_LOCATION_ERR,PETSC_TRUE);CHKERRQ(ierr);
-  return 0;
-}
-
-PetscErrorCode IFunctionHeat(TS ts,PetscReal t,Vec X,Vec Xdot,Vec r,void *ctx)
-{
-  AppCtx         *appctx = (AppCtx*)ctx;     /* user-defined application context */
-  PetscErrorCode ierr;
-
-  ierr = MatMult(appctx->A,X,r);CHKERRQ(ierr);
-  ierr = VecAYPX(r,-1.0,Xdot);CHKERRQ(ierr);
-  return 0;
-}
-
-PetscErrorCode IJacobianHeat(TS ts,PetscReal t,Vec X,Vec Xdot,PetscReal s,Mat A,Mat B,void *ctx)
-{
-  AppCtx         *appctx = (AppCtx*)ctx;     /* user-defined application context */
-  PetscErrorCode ierr;
-
-  if (appctx->oshift == s) return 0;
-  ierr = MatCopy(appctx->A,A,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
-  ierr = MatScale(A,-1);CHKERRQ(ierr);
-  ierr = MatShift(A,s);CHKERRQ(ierr);
-  ierr = MatCopy(A,B,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
-  appctx->oshift = s;
   return 0;
 }
